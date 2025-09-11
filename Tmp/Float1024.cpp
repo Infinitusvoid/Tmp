f7d764d3b785f7b876012a3dfcaf737e1fd48944
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2025-09-11 11:15:57 +0200

    update

--- Tmp/Float1024.cpp
+++ Tmp/Float1024.cpp
@@ -14,15 +14,44 @@ namespace Float1024_
 	{
 		CompositionFuncton function;
 
-		int index_0 = 0;
-		int index_1 = 0;
-		int index_2 = 0;
+		int index_0 = -1;
+		int index_1 = -1;
+		int index_2 = -1;
 	};
 
-	std::vector<Composition> compositions;
+	
+
+	class Engine
+	{
+	public:
+		Engine()
+		{
+			compositions.push_back(Composition(CompositionFuncton::BASE, -1, -1, -1));
+		}
+
+		~Engine()
+		{
+			compositions.clear();
+		}
+
+		int clone(int index)
+		{
+			Composition el = compositions.at(index);
+			compositions.push_back(el);
+		}
+
+		
+
+	private:
+
+		std::vector<Composition> compositions;
+	};
 
 	void build()
 	{
+		Engine engine;
+
+		
 
 	}
 
