7628dbdeaa0742386872caf28f8e0a1c88e05a20
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2025-09-13 18:51:20 +0200

    update

--- Tmp/main.cpp
+++ Tmp/main.cpp
@@ -2,17 +2,23 @@
 #include "CppCommponents/File.h"
 
 
+
 #include <iostream>
 
 #include "Scene.h"
 
 
+const std::string folder_ouput = "C:/Users/Cosmos/Desktop/output/tmp/";
+const std::string folder_output_shaders = folder_ouput + "shaders/";
+const std::string folder_output_frames = folder_ouput + "frames/";
 
 int main(int argc, char* argv[])
 {
     std::cout << "Tmp\n";
     
-    
+    Folder::create_folder_if_does_not_exist_already(folder_ouput);
+    Folder::create_folder_if_does_not_exist_already(folder_output_shaders);
+    Folder::create_folder_if_does_not_exist_already(folder_output_frames);
     
     
     
